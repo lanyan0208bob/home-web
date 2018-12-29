46060b8fb63c6ab0b1aff02e699b6c5a150b2c74
wangjibo <1179337719@qq.com>
2018-12-29 18:24:15 +0800

    结构整理

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,6 +1,6 @@
-//基本路径
-var baseUrl = 'http://localhost:8008/home-web/';
 
+//基本路径-服务端
+var baseUrl = 'http://localhost:8008/home-webapp/';
 
 /*
 *请求json数据到服务器
@@ -17,19 +17,19 @@ function requestJsonServer(jsonData, url) {
   });
 }
   /*
-  *请求json数据到服务器
+  *请求form数据到服务器
    */
   function requestServer(ops) {
     console.info("进入请求");
     var url=baseUrl+ops.url;
    console.info("url:"+url);
     console.info("data:"+ops.data);
-    var jsonString=JSON.stringify(ops);
-
     $.ajax({
       type: 'POST',
       url: url,
       data: ops.data,
+      dataType: "json",
+      jsonp: "callback",
       async: false,
       success:function (data) {
         console.info("return：" + data);
